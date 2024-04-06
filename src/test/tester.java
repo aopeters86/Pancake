c376301508ce4f561883c2118202440ce1e3f925
aopeters86 <114446425+aopeters86@users.noreply.github.com>
2024-04-06 09:04:51 -0500

    not much?

--- src/test/tester.java
+++ src/test/tester.java
@@ -1,7 +1,13 @@
 package test;
-
 import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Properties;
 
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.util.CoreMap;
 import machine.PowerOnPancakeMaker;
 
 public class tester {
@@ -14,5 +20,25 @@ public class tester {
 		
 		PowerOnPancakeMaker  newPCM1 = new PowerOnPancakeMaker();
 		newPCM1.on();//on switch
-		}
+	
+	Properties props = new Properties();
+	props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, sentiment");
+	StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+	String text = "I love this product! It's amazing.";
+	Annotation document = new Annotation(text);
+	pipeline.annotate(document);
+	List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+	for (CoreMap sentence : sentences) {
+	    String sentiment = sentence.get(SentimentCoreAnnotations.SentimentClass.class);
+	    System.out.println("Sentiment of the sentence: " + sentiment);
+	}
+
+	}
+
+	
+
+	
+	
 }
+
+
